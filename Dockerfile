d12e27651dc0c94156c89c7081ed09f8e70a9bf7
Abdur Rofi <rofim58@gmail.com>
2024-06-11 12:58:24 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,50 @@
 # Use an official PHP runtime as a parent image
-FROM php:8.4-fpm
-
+FROM php:8.3.8-fpm-alpine
+ENV COMPOSER_ALLOW_SUPERUSER=1
 # Install system dependencies
-RUN apt-get update && apt-get install -y \
-    supervisor \
-    && docker-php-ext-install pdo_mysql
+RUN apk update
+RUN apk add --no-cache \
+    bash \
+    curl \
+    libpng-dev \
+    libjpeg-turbo-dev \
+    libwebp-dev \
+    libxpm-dev \
+    libvpx-dev \
+    libzip-dev \
+    icu-dev \
+    zlib-dev \
+    busybox-extras
+
+RUN docker-php-ext-configure gd --with-jpeg --with-webp --with-xpm
+RUN docker-php-ext-configure intl
+RUN docker-php-ext-install gd pdo_mysql zip intl
+
 
 # Install composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Install Node.js and NPM
+RUN apk add --no-cache nodejs yarn
 
 # Set working directory
 WORKDIR /var/www
 
 # Copy application source code
-COPY . .
+COPY composer.json composer.lock ./
 
 # Install application dependencies
-RUN composer install --no-dev --no-scripts --no-interaction
+RUN composer install --no-dev --optimize-autoloader --no-scripts --ignore-platform-req=ext-zip
+RUN yarn install
 
-# Copy supervisor configuration
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+# Copy application source code
+COPY . .
 
 # Copy the entrypoint script
 COPY docker-entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/docker-entrypoint.sh
+
+RUN chown -R www-data:www-data /var/www
 
 # Expose port 9000 and start php-fpm server
 EXPOSE 9000
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,76 @@
 #!/bin/sh
 
-# Function to check if a table exists
-check_table_exists() {
-    php -r "
-    \$connection = new PDO('mysql:host='.\$_SERVER['DB_HOST'].';dbname='.\$_SERVER['DB_DATABASE'], \$_SERVER['DB_USERNAME'], \$_SERVER['DB_PASSWORD']);
-    \$result = \$connection->query(\"SHOW TABLES LIKE 'migrations'\")->rowCount();
-    exit(\$result ? 0 : 1);
-    "
+# Function to check MySQL connection using telnet
+check_mysql_connection() {
+    source /var/www/.env
+    telnet $DB_HOST $DB_PORT > /dev/null 2>&1
+    return $?
 }
 
-# Check if migrations table exists, and if not, run migrations
-if check_table_exists; then
+# Set environment variables
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
+# Clear and cache configuration
+echo "Clearing and caching config..."
+php artisan config:clear
+php artisan config:cache
+php artisan route:clear
+php artisan view:clear
+
+# Ensure proper permissions for storage and bootstrap/cache directories
+echo "Setting permissions for storage and bootstrap/cache directories..."
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 777 /var/www/storage /var/www/bootstrap/cache
+
+# Check if storage/logs/laravel.log exists and set permissions
+if [ ! -f /var/www/storage/logs/laravel.log ]; then
+    touch /var/www/storage/logs/laravel.log
+    chown www-data:www-data /var/www/storage/logs/laravel.log
+    chmod 777 /var/www/storage/logs/laravel.log
+fi
+
+# Check MySQL connection and wait if necessary
+echo "Checking MySQL connection..."
+mysql_attempts=0
+max_attempts=5
+while ! check_mysql_connection; do
+    mysql_attempts=$((mysql_attempts+1))
+    if [ $mysql_attempts -ge $max_attempts ]; then
+        echo "Unable to connect to MySQL after $max_attempts attempts."
+        exit 1
+    fi
+    echo "MySQL connection failed. Retrying in 5 seconds..."
+    sleep 5
+done
+echo "MySQL connection established."
+
+# Function to check if a table exists
+check_migration_status() {
+    php artisan migrate:status > /dev/null 2>&1
+    return $?
+}
+
+# Check the migration status, and if no migrations have been run, run migrations and seed the database
+echo "Checking migration status..."
+check_migration_status
+if [ $? -eq 0 ]; then
     echo "Migrations table exists. Skipping migration."
 else
-    echo "Migrations table does not exist. Running migrations."
+    echo "Migrations table does not exist or there is an issue. Running migrations."
     php artisan migrate --force
     php artisan db:seed --force
 fi
 
 # Generate the application key (only if not already set)
 if [ -z "$APP_KEY" ]; then
+    echo "Generating application key..."
     php artisan key:generate --force
 fi
 
-# Start supervisord
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf "$@"
+# Start php-fpm
+echo "Starting PHP-FPM..."
+php-fpm
+
+# Keep the container running by tailing the PHP-FPM log
+tail -f /var/log/php7.4-fpm.log
